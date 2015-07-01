2014d1fd41b989c572e6700dbbce78d94d6737c4
Tyler Ferguson <tferguson@scottlogic.co.uk>
2015-07-01 16:07:47 +0100

    Created another protractor test - testing model matches binding

--- spec/e2e/tests/spec.js
+++ spec/e2e/tests/spec.js
@@ -1,8 +1,20 @@
 // spec.js
 describe('Mobile', function() {
-    it('should have a title', function() {
-        browser.get('http://localhost:63342/AngularSetup/app/index.html');
 
+    beforeEach(function() {
+        browser.get('http://localhost:63342/AngularSetup/app/index.html');
+    });
+
+    it('should have a title', function() {
         expect(browser.getTitle()).toEqual('Angular Mobile Setup');
+    });
+
+    it('copies and outputs your input', function() {
+        var typedMessage = element(by.model('typedMessage'));
+        var outputMessage = element(by.binding('typedMessage'));
+
+        var testMessage = 'This is pretty cool!';
+        typedMessage.sendKeys(testMessage);
+        expect(outputMessage.getText()).toBe(testMessage);
     });
 });
